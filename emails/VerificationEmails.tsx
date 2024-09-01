643108b9c1a824ca15286213a0f232d120fbb790
Divyanshu Rajput <dvnhrajput@gmail.com>
2024-09-01 21:14:19 +0530

    Add Resend Email Service

---
+++ emails/VerificationEmails.tsx
@@ -0,0 +1,55 @@
+import {
+    Html,
+    Head,
+    Font,
+    Preview,
+    Heading,
+    Row,
+    Section,
+    Text,
+    Button
+} from '@react-email/components'
+
+interface VerificationEmailProps {
+    username: string;
+    otp: string
+}
+
+export default function VerificationEmail({ username, otp }: VerificationEmailProps) {
+    return (
+        <Html lang='en' dir='ltr'>
+            <Head>
+                <title>Verification Code</title>
+                <Font
+                    fontFamily='Roboto'
+                    fallbackFontFamily='Verdana'
+                    webFont={{
+                        url: 'https://fonts.gstatic.com/s/roboto/v27/KF0mCnqEu92Fr1Mu4mxKKTU1Kg.woff2',
+                        format: 'woff2'
+                    }}
+                    fontWeight={400}
+                    fontStyle='normal'
+                />
+                <Preview>Here&apos;s your verification code: {otp}</Preview>
+                <Section>
+                    <Row>
+                        <Heading as='h2'>Hello {username},</Heading>
+                    </Row>
+                    <Row>
+                        <Text>
+                            Thank you for registering.Please use the following verification code to complete your registration:
+                        </Text>
+                    </Row>
+                    <Row>
+                        <Text>{otp}</Text>
+                    </Row>
+                    <Row>
+                        <Text>
+                            If you did not request this code, please ignore this email.
+                        </Text>
+                    </Row>
+                </Section>
+            </Head>
+        </Html>
+    )
+}
