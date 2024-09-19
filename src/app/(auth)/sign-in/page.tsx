5201b40d7342e3266311a4d6e81c2236122c32ad
Divyanshu Rajput <dvnhrajput@gmail.com>
2024-09-19 06:24:03 +0530

    sign-up Complete

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,11 @@
-'use client'
-import {useSession, signIn, signOut} from 'next-auth/react'
+import React from 'react'
 
-export default function Component(){
-    const {data: session} = useSession()
-    if(session){
-        return (
-            <>
-            Signed in as {session.user.email} <br />
-            <button onClick={()=>signOut()}>Sign Out</button>
-            </>
-        )
-    }
-
-    return (
-        <>
-        Not signed in  <br />
-        <button className='bg-orange-500 px-3 py-1 m-4 rounded' onClick={()=>signIn()}>SignIn</button>
-        </>
-    )
+function page() {
+  return (
+    <div>
+      SignIn
+    </div>
+  )
 }
+
+export default page
---
+++ src/app/api/suggest-messages/route.ts
@@ -0,0 +1,29 @@
+// import { openai } from '@ai-sdk/openai';
+// import { streamText, convertToCoreMessages } from 'ai';
+// import { NextResponse } from 'next/server';
+// import OpenAI from 'openai';
+
+// // Allow streaming responses up to 30 seconds
+// export const maxDuration = 30;
+
+// export async function POST(req: Request) {
+//  try {
+//     const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output shouldbe structured like this: 'What's a hobby you've recently started?||If you could have dinner with any historical figure, who would it be?||What's a simploe thing that makes you happy?'.Ensure the questions are intriguing, foster curiosity, and contirbute to a positive and welcoming conversational environment."
+
+//     const result = await streamText({
+//       model: openai('gpt-4-turbo'),
+//       messages: convertToCoreMessages(prompt),
+//     });
+  
+//     return result.toDataStreamResponse();
+//  } catch (error) {
+//     if (error instanceof OpenAI.APIError){
+//         const {name, status, headers, message} = error
+//         return NextResponse.json({
+//             name, status, headers, message
+//         }, {status})
+//     } else {
+//         console.error("Unexpected error occured", error)
+//     }
+//  }
+// }
